9db87548d2933345896a35d32e5257fb67117bc9
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-12-14 14:02:05 +0600

    test

--- .next/types/app/blogs/page.ts
+++ .next/types/app/blogs/page.ts
@@ -1,4 +1,4 @@
-// File: /Volumes/www/main-website/app/blogs/page.jsx
+// File: /home/sakib/work/main-website/app/blogs/page.jsx
 import * as entry from '../../../../app/blogs/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
