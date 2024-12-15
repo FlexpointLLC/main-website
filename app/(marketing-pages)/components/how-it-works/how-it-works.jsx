5f1103693dfd0e2ca98c0e832b45e0e0a63a6905
muntashir-wahid <muntashir.wahid@yahoo.com>
2024-12-15 10:31:17 +0600

    created how it works section

--- app/(marketing-pages)/components/how-it-works/how-it-works.jsx
+++ app/(marketing-pages)/components/how-it-works/how-it-works.jsx
@@ -1,6 +1,37 @@
+import Image from "next/image";
+
+import NoCodeImg from "@/public/assets/img/no-code.svg";
+import SeamlessCheckoutImg from "@/public/assets/img/seamless-checkout.svg";
+import AppIntegrationsImg from "@/public/assets/img/app-integrations.svg";
+
 import SectionHeading from "../section-heading/section-heading";
 
-const workingStrategies = [{ id: "1" }, { id: "2" }, { id: "3" }];
+const workingStrategies = [
+  {
+    id: "1",
+    subTitle: "Get Started in Minutes",
+    title: "No Code, No Hassle",
+    shortDes:
+      "Get started quickly without writing a single line of code. Build your store in minutes and focus on growing your business!",
+    image: NoCodeImg,
+  },
+  {
+    id: "2",
+    subTitle: "Boost Your Sales",
+    title: "Seamless 1-Tap Checkout",
+    shortDes:
+      "Make it easy for your customers to purchase with just one tap. Flexpoint optimizes your checkout process to increase conversions and reduce friction.",
+    image: SeamlessCheckoutImg,
+  },
+  {
+    id: "3",
+    subTitle: "Connect Your Tools",
+    title: "Effortless App Integrations",
+    shortDes:
+      "Connect with all the tools you love. Flexpoint integrates with the top apps to keep your business running smoothly.",
+    image: AppIntegrationsImg,
+  },
+];
 
 const HowItWorks = () => {
   return (
@@ -16,10 +47,25 @@ const HowItWorks = () => {
           {workingStrategies.map((item) => (
             <article
               key={item.id}
-              className="rounded-2xl border border-[#E1E4EA]"
+              className="flex flex-col-reverse rounded-2xl border border-[#E1E4EA] md:flex-row"
             >
-              <div className="h-40"></div>
-              <figure></figure>
+              <div className="w-full p-4 md:w-3/5 md:p-12">
+                <span className="mb-1 text-[12px] font-medium uppercase text-[#5C5C5C]">
+                  {item.subTitle}
+                </span>
+                <h4 className="mb-4 text-2xl font-medium text-heading">
+                  {item.title}
+                </h4>
+                <p className="mb- mb-8 text-[#5C5C5C]">{item.shortDes}</p>
+              </div>
+
+              <figure className="w-full md:w-2/5">
+                <Image
+                  src={item.image}
+                  alt={`${item.title} image`}
+                  className="w-full"
+                />
+              </figure>
             </article>
           ))}
         </div>
