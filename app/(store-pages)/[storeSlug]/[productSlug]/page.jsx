b371616cf50a803d72941de366cd104cae202d38
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-12-17 14:31:39 +0600

    add product page component; implement data fetching and error handling

---
+++ app/(store-pages)/[storeSlug]/[productSlug]/page.jsx
@@ -0,0 +1,40 @@
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import storeFooterLogo from "@/public/assets/img/store-footer-logo.svg";
+
+const base_url = process.env.NEXT_PUBLIC_API_URL;
+
+export default async function ProductPage({ params }) {
+  console.log(params);
+  const storeSlug = params.storeSlug;
+  const productSlug = params.productSlug;
+
+  const response = await fetch(
+    `${base_url}/${storeSlug}/product/${productSlug}`,
+    {
+      cache: "no-store",
+    },
+  )
+    .then((res) => (res.ok ? res.json() : Promise.reject(res.statusText)))
+    .catch((err) => {
+      console.error("Error fetching product data:", err);
+      return null;
+    });
+
+  if (!response || response?.message === "Product not found") {
+    return redirect("/");
+  }
+
+  const product = response?.data || {};
+
+  console.dir(product, { depth: null });
+
+  return (
+    <div className="mx-auto max-w-[375px] px-4 py-12">
+      <footer className="mt-6 flex items-center justify-center gap-[7px]">
+        <p className="pl-4 text-xs font-medium text-para">Powered by</p>
+        <Image src={storeFooterLogo} alt="store footer logo" />
+      </footer>
+    </div>
+  );
+}
