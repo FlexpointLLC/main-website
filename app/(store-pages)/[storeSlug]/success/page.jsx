36fd049aa4f33b58059063d46a6e603a5465c532
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-12-30 20:56:42 +0600

    feat: enhance confirmation modal with navigation and footer branding

--- app/(store-pages)/[storeSlug]/success/page.jsx
+++ app/(store-pages)/[storeSlug]/success/page.jsx
@@ -1,37 +1,51 @@
+"use client";
+
+import storeFooterLogo from "@/public/assets/img/store-footer-logo.svg";
 import { AlertDialog, AlertDialogContent } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 
 import congratulationSvg from "@/public/assets/success/congratulations.svg";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-export default function ConfirmationModal() {
+export default function ConfirmationModal({ params }) {
+  const storeSlug = params.storeSlug;
+  const router = useRouter();
   return (
-    <AlertDialog open={true}>
-      <AlertDialogContent className="gap-0 p-0">
-        <Image
-          src={congratulationSvg}
-          alt="congratulation"
-          className="w-full"
-        />
-        <div className="p-5 pt-0">
-          <h2 className="mb-2 text-center text-2xl font-medium text-[#0E121B]">
-            Congratulations! 🥳
-          </h2>
-          <p className="mx-auto max-w-96 text-center text-sm text-[#525866]">
-            {`You'll receive a confirmation email shortly!`}
-          </p>
-        </div>
-        <hr />
-        <div className="px-5 py-4">
-          <Button
-            // onClick={() => setOpenCongratulationModal(false)}
+    <div className="relative h-screen">
+      {" "}
+      <AlertDialog open={true}>
+        <AlertDialogContent className="gap-0 p-0">
+          <Image
+            src={congratulationSvg}
+            alt="congratulation"
             className="w-full"
-            variant={"outline"}
-          >
-            Close
-          </Button>
-        </div>
-      </AlertDialogContent>
-    </AlertDialog>
+          />
+          <div className="p-5 pt-0">
+            <h2 className="mb-2 text-center text-2xl font-medium text-[#0E121B]">
+              Congratulations! 🥳
+            </h2>
+            <p className="mx-auto max-w-96 text-center text-sm text-[#525866]">
+              {`You'll receive a confirmation email shortly!`}
+            </p>
+          </div>
+          <hr />
+          <hr />
+          <div className="px-5 py-4">
+            <Button
+              onClick={() => router.push(`/${storeSlug}`)}
+              className="w-full"
+              variant={"outline"}
+            >
+              Return to home
+            </Button>
+          </div>
+        </AlertDialogContent>
+      </AlertDialog>
+      <footer className="absolute bottom-0 my-6 flex w-full items-center justify-center gap-[7px]">
+        <p className="pl-4 text-xs font-medium text-para">Powered by</p>
+        <Image src={storeFooterLogo} alt="store footer logo" />
+      </footer>
+    </div>
   );
 }
