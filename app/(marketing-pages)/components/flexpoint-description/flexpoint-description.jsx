996e1c174606142201cc63fb8b41644b05d4ec24
dev-maruful <maruful.islam401@gmail.com>
2025-01-01 23:48:22 +0600

    Added description section desktop view

--- app/(marketing-pages)/components/flexpoint-description/flexpoint-description.jsx
+++ app/(marketing-pages)/components/flexpoint-description/flexpoint-description.jsx
@@ -1,9 +1,11 @@
 const FlexpointDescription = () => {
   return (
-    <section className="bg-gradient-secondary-from-bottom">
-      <div className="space-y -3 w-full px-4 py-20 text-center md:mx-auto md:max-w-4xl md:py-36">
-        <h2 className="font-semibold text-success-base">What is flexpoint?</h2>
-        <p className="text-4xl font-semibold text-heading md:text-5xl">
+    <section className="bg-gradient-to-b from-white to-[#F0FAFA]">
+      <div className="w-full space-y-[10px] px-4 py-20 text-center md:mx-auto md:max-w-4xl md:pb-[148px] md:pt-[22px]">
+        <h2 className="font-semibold leading-6 tracking-[-0.18px] text-success-base">
+          What is flexpoint?
+        </h2>
+        <p className="text-4xl font-semibold text-heading md:text-5xl md:leading-[58px]">
           We get it.{" "}
           <span className="text-[#99A0AE]">
             Making money as a creator is tough, and standing out is expensive.
