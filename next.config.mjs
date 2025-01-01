d2df30621dda39edeee90d024d3a7a8a3057419a
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2025-01-01 23:46:58 +0600

    Update Next.js configuration and .gitignore for build directory and environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  distDir: "_next",
+  images: {
+    domains: ["dev-api.flexpoint.store"],
+  },
+};
 
 export default nextConfig;
