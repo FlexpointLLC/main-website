9799e39b52a3ad2fd6fec24494e9e1c83afaf167
muntashir-wahid <muntashir.wahid@yahoo.com>
2025-01-30 16:04:24 +0600

    removed unused variables

--- app/(store-pages)/[storeSlug]/[productSlug]/_components/slot-picker.jsx
+++ app/(store-pages)/[storeSlug]/[productSlug]/_components/slot-picker.jsx
@@ -12,12 +12,6 @@ const SlotPicker = ({
   productSlug,
   availableSlots: formattedAvailableSlots,
 }) => {
-  // const { data: availableSlots, isLoading: isSlotsLoading } =
-  //   useGetAvailableSlotsQuery({
-  //     productSlug,
-  //     selectedDate: moment(selectedDate).format("YYYY-MM-DD"),
-  //   });
-
   if (!formattedAvailableSlots) {
     return (
       <div className="max-h-[280px] animate-pulse overflow-y-auto rounded-lg border border-[#F2F5F8] bg-white p-4 scrollbar-none">
