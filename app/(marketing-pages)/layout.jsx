b1f50e738850686512e6ede69b126b79248449cf
sakib-xrz <sakibxrz21@gmail.com>
2025-03-02 11:40:00 +0600

    Restore Footer component in marketing pages layout and root layout

--- app/(marketing-pages)/layout.jsx
+++ app/(marketing-pages)/layout.jsx
@@ -6,7 +6,7 @@ const MarketingPagesLayout = ({ children }) => {
     <main className="min-h-screen bg-white">
       <Navbar />
       {children}
-      {/* <Footer /> */}
+      <Footer />
     </main>
   );
 };
--- app/layout.jsx
+++ app/layout.jsx
@@ -5,7 +5,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import GlobalProvider from "@/components/global/global-providers/global-provider";
-import Footer from "@/components/global/footer/footer";
 
 const inter = Inter({
   weight: ["400", "500", "600", "700", "800", "900"],
@@ -22,7 +21,6 @@ export default function RootLayout({ children }) {
     <html lang="en" className="scroll-smooth">
       <body className={cn(inter.className)}>
         <GlobalProvider>{children}</GlobalProvider>
-        <Footer />
       </body>
     </html>
   );
