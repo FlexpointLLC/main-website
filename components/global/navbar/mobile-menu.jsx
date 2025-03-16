478dfea748fe2e09245518d03390fca1846cd63f
muntashir-wahid <muntashir.wahid@yahoo.com>
2025-03-16 11:20:40 +0600

    fixed redirection link issue

--- components/global/navbar/mobile-menu.jsx
+++ components/global/navbar/mobile-menu.jsx
@@ -29,7 +29,7 @@ export default function MobileMenu({ openMenuBar, setOpenMenuBar }) {
     },
     {
       name: "Login",
-      href: "https://dev-admin.flexpoint.store/lookup",
+      href: "https://admin.flexpoint.store/lookup",
     },
   ];
 
