c5010075030cee3935b1b91d6e5015a77b3e8522
Ashik Prottoy <30628683+ashikbd1100@users.noreply.github.com>
2025-11-07 20:08:43 +0600

    Fix: Only initialize Redux Provider on client side to prevent SSR issues

--- components/global/global-providers/global-provider.jsx
+++ components/global/global-providers/global-provider.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { store } from "@/redux/store";
 
 import { Provider } from "react-redux";
@@ -7,6 +8,22 @@ import { Provider } from "react-redux";
 import { Toaster } from "sonner";
 
 export default function GlobalProvider({ children }) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  // Only render Redux Provider on client side to avoid SSR issues
+  if (!isClient) {
+    return (
+      <>
+        <Toaster position="top-center" richColors />
+        {children}
+      </>
+    );
+  }
+
   return (
     <Provider store={store}>
       <Toaster position="top-center" richColors />
